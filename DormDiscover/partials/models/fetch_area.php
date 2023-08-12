58d9dd1e31edcffdf78cebabe88093184d53dd2c
AMAN ANSARI <76636948+amanansari12@users.noreply.github.com>
2023-08-12 12:38:26 +0530

    Delete DormDiscover directory

--- DormDiscover/partials/models/fetch_area.php
+++
@@ -1,6 +0,0 @@
-<?php
-$sql_area = "SELECT area_name FROM arearnc WHERE areaId =' $area_id' ";
-$result2 = mysqli_query($conn, $sql_area);
-$area_row = mysqli_fetch_assoc($result2);
-
-$areaname = $area_row['area_name'];
--- DormDiscover/partials/models/users.php
+++
@@ -1,4 +0,0 @@
-<?php
-$fetch_user = "SELECT * FROM users WHERE user_id = '$user_id' ";
-$user_result = mysqli_query($conn, $fetch_user);
-$user_count = mysqli_num_rows($user_result);
